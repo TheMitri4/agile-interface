8c8cefc8a517e43e5ceb0556757524f359910f87
TheMitri4 <DimaMK98@gmail.com>
2019-04-09 02:39:59 +0300

    localstorage

--- main.js
+++ main.js
@@ -8,11 +8,13 @@ var getSprintStoryPoints = document.querySelector('.addtask__input[name="sprint-
 
 function Agile() {
 	this.backlog = [];
+	this.sprint = [];
 }
 
 Agile.prototype.addTask = function(task){
 	this.backlog.push(task);
-	displayTask(task);
+	displayTask(task, backlogItemsWrapper);
+	saveToLocalStorage(this.backlog, 'backlogLocal');	
 };
 
 Agile.prototype.addUrgentTask = function(task){
@@ -20,11 +22,15 @@ Agile.prototype.addUrgentTask = function(task){
 };
 
 Agile.prototype.getSprint = function(storyPoints){
-	var sprint = [];
+	this.sprint = [];
 	var max = +storyPoints;
+	sprintItemsWrapper.childNodes.forEach(item => item.classList.add('task__hidden'));
+	setTimeout(function(){
+		sprintItemsWrapper.innerHTML = '';
+	}, 400);
 	this.backlog = this.backlog.reduce((acc,item,i) => {
 		if(item.storyPoints <= max){
-			sprint.push(item);
+			this.sprint.push(item);
 			max -= item.storyPoints;
 			taskToSprint(i);
 		}
@@ -33,8 +39,9 @@ Agile.prototype.getSprint = function(storyPoints){
 		}
 		return acc;
 	}, []);
-			
-	return sprint;
+	saveToLocalStorage(this.backlog, 'backlogLocal');
+	saveToLocalStorage(this.sprint, 'sprintLocal');
+	return this.sprint;
 }
 
 function Task(title, storyPoints) {
@@ -43,13 +50,21 @@ function Task(title, storyPoints) {
 }
 
 var agile = new Agile;
+agile.backlog = JSON.parse(localStorage.getItem('backlogLocal')) || [];
+if(agile.backlog.length > 0){
+	agile.backlog.forEach(item => displayTask(item, backlogItemsWrapper));
+}
+agile.sprint = JSON.parse(localStorage.getItem('sprintLocal')) || [];
+if(agile.sprint.length > 0){
+	agile.sprint.forEach(item => displayTask(item, sprintItemsWrapper));
+}
 
-function displayTask(task){
+function displayTask(task, destination){
 	var item = document.createElement('div');
 	item.classList.add('task');
 	item.classList.add('task__hidden');
-	item.innerHTML = `<h3 class="task__title">${task.title}</h3><p class="task__difficulty">Сложность:</p><p class="task__storypoints">${task.storyPoints}</p>`;
-	backlogItemsWrapper.appendChild(item);
+	item.innerHTML = `<h3 class="task__title">${task.title}</h3><p class="task__difficulty">Сложность:</p><p class="task__storypoints">${task.storyPoints}</p><button class="task__remove"></button>`;
+	destination.appendChild(item);
 	setTimeout(function(){
 		item.classList.remove('task__hidden');
 	}, 20);
@@ -61,13 +76,14 @@ function taskToSprint(i){
 	setTimeout(function(){
 		sprintItemsWrapper.appendChild(backlogTasks[i]);
 	}, 400);
-	var removeButton = document.createElement('button');
-	removeButton.classList.add('task__remove');
-	backlogTasks[i].appendChild(removeButton);
-	removeTask(removeButton);
 	setTimeout(function(){
 		backlogTasks[i].classList.remove('task__hidden');
 	}, 500);
+}
+
+function saveToLocalStorage(item, name){
+	var itemJson = JSON.stringify(item);
+	localStorage.setItem(name, itemJson);
 }
 
 addTaskButton.addEventListener('click', event => {
@@ -90,16 +106,30 @@ getSprintButton.addEventListener('click', event => {
 	getSprintStoryPoints.value = '';
 });
 
+backlogItemsWrapper.addEventListener('click', event => {
+	if(!event.target.classList.contains('task__remove')){
+		return;
+	}
+	var backlogTasks = backlogItemsWrapper.querySelectorAll('.task');
+	agile.backlog.splice(Array.prototype.indexOf.call(backlogTasks, event.target.parentNode), 1);
+	event.target.parentNode.classList.add('task__hidden');
+	setTimeout(function(){
+		backlogItemsWrapper.removeChild(event.target.parentNode);
+	}, 400);
+	saveToLocalStorage(agile.backlog, 'backlogLocal');
+});
 
-function removeTask(item){
-	item.addEventListener('click', event => {
-		event.preventDefault();
-		item.parentNode.classList.add('task__hidden');
-		setTimeout(function(){
-			sprintItemsWrapper.removeChild(item.parentNode);
-		},400);
-	});
-}
-
+sprintItemsWrapper.addEventListener('click', event => {
+	if(!event.target.classList.contains('task__remove')){
+		return;
+	}
+	var sprintTasks = sprintItemsWrapper.querySelectorAll('.task');
+	agile.sprint.splice(Array.prototype.indexOf.call(sprintTasks, event.target.parentNode), 1);
+	event.target.parentNode.classList.add('task__hidden');
+	setTimeout(function(){
+		sprintItemsWrapper.removeChild(event.target.parentNode);
+	}, 400);
+	saveToLocalStorage(agile.sprint, 'sprintLocal');
+});
 
 
